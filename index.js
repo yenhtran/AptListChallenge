62348f06bcf16d1413bcf7f2dc863aa63623ad5d
Yen Tran <yen.tran@autodesk.com>
2017-05-11 11:19:07 -0700

    shuffles names in a list

--- index.js
+++ index.js
@@ -13,3 +13,52 @@ console.log(
         figlet.textSync('Family Friday!', { horizontalLayout: 'full' })
     )
 );
+
+var entireCompany = ['Happy','Dopey','Grumpy','Sneezy','Bashful','Sleepy','Doc','Snow','Super Woman','Batman'];
+
+
+function shuffle(array) {
+
+}
+
+function makeGroups(entireCompany) {
+	console.log('making groups!');
+
+	//If the length of the entireCompany is less than 3, return error
+	//Take array and shuffle the names
+	//Break the list of names into chunks of 4 people per group
+	//Validate groups
+	//	if last group size is less than 3
+	//		take everyone in that last group and pop each one off...
+	//		(Loop through the brokenUpList)
+	//				(if the length of the current chunk is less than 4)
+	//						...add 1 person to each of the other groups
+	//
+
+	if (entireCompany.length < 3) return new Error(['not enough people']);
+
+	var shuffledNames = [],
+			num = entireCompany.length,
+			i;
+
+			while (num) {
+				i = Math.floor(Math.random() * entireCompany.length);
+
+				if (i in entireCompany) {
+					shuffledNames.push(entireCompany[i]);
+					delete entireCompany[i];
+					num--;
+				}
+			}
+			return shuffledNames;
+}
+
+console.log(makeGroups(entireCompany));
+
+
+
+
+
+
+
+
