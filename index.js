b7073399b153d206c20bb49329db3c749309cca8
Yen Tran <yen.tran@autodesk.com>
2017-05-11 15:49:01 -0700

    new names can written to input.txt file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var chalk = require('chalk'),
     clear = require('clear'),
     figlet = require('figlet'),
+    fs = require('fs'),
+    readline = require('readline'),
     groupProcessor = require('./lib/groupProcessor');
 
 /*
@@ -17,4 +19,18 @@ console.log(
 
 var entireCompany = ['Happy', 'Dopey', 'Grumpy', 'Sneezy', 'Bashful', 'Sleepy', 'Doc', 'Snow', 'Super Woman', 'Batman', 'Superman', 'Thor', 'Loki'];
 
+
+process.stdin.on('data', function(data) {
+	fs.appendFile('data/input.txt', data, function(err) {
+		if (err) throw err;
+		process.exit()
+	});
+});
+
+function beginProgram(i) {
+    process.stdout.write(`Please enter the name of your new friend!\n`);
+    process.stdout.write("   >   ");
+}
+
 console.log(groupProcessor.makeGroups(entireCompany));
+beginProgram(0);
