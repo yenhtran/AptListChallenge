430646be64823c769708b520df37b247a1d3544b
Yen Tran <yen.tran@autodesk.com>
2017-05-11 15:33:47 -0700

    adds tests to core functionality

--- test/groupProcessor.test.js
+++ test/groupProcessor.test.js
@@ -7,21 +7,66 @@ var chai = require('chai'),
     groupProcessor = require('../lib/groupProcessor.js');
 
 describe('groupProcessor', function() {
-	describe('#shuffle', function() {
-		it('should return an array with names in a different order');
-	});
+    var groupProcessorRewire = rewire('../lib/groupProcessor.js'),
+        entireCompany;
 
-	describe('#breakIntoChunks', function() {
-		it('should return a list of names broken into chunks of 4 names per chunk.');
-	});
+    beforeEach(function() {
+        entireCompany = ['Happy', 'Dopey', 'Grumpy', 'Sneezy', 'Bashful', 'Sleepy', 'Doc', 'Snow', 'Thor'];
+    });
 
-	describe('#validateGroup', function() {
-		it('should not have groups with less than 3 names per group.');
-		it('should not have groups with more than 5 names per group');
-	});
+    describe('#shuffle', function() {
+        var private_shuffle = groupProcessorRewire.__get__('shuffle'),
+            shuffledList;
 
-	describe('#makeGroups', function() {
-		it('should return an error if a group that is less than 3 names is submitted.');
-		it('should return a shuffled group that has no more than 5 people, and no less than 3 people per group');
-	});
+        it('should return an array with names in a different order', function() {
+            shuffledList = private_shuffle(entireCompany);
+
+            expect(shuffledList).to.not.equal(['Happy', 'Dopey', 'Grumpy', 'Sneezy', 'Bashful', 'Sleepy', 'Doc', 'Snow', 'Thor']);
+        });
+    });
+
+    describe('#breakIntoChunks', function() {
+    		var private_breakIntoChunks = groupProcessorRewire.__get__('breakIntoChunks'),
+            brokenUpList;
+
+        it('should return a list of names broken into chunks of 4 names per chunk.', function() {
+        		brokenUpList = private_breakIntoChunks(entireCompany);
+
+            expect(brokenUpList[0].length).to.equal(4);
+            expect(brokenUpList[1].length).to.equal(4);
+        });
+    });
+
+    describe('#validateGroup', function() {
+    		var private_validateGroup = groupProcessorRewire.__get__('validateGroup'),
+            brokenUpList = [['Happy','Dopey','Grumpy','Sneezy'], ['Bashful', 'Sleepy','Doc','Snow'],['Thor','Loki']],
+            validatedGroup = private_validateGroup(brokenUpList);
+
+        it('should not have groups with less than 3 names per group.', function() {
+        		expect(validatedGroup.length).be.equal(2);
+            expect(validatedGroup[0].length).be.equal(5);
+            expect(validatedGroup[1].length).be.equal(5);
+        });
+        it('should not have groups with more than 5 names per group', function() {
+        		expect(validatedGroup[0].length).to.be.at.most(5);
+            expect(validatedGroup[1].length).to.be.at.most(5);
+        });
+    });
+
+    describe('#makeGroups', function() {
+    		var entireStartUp = ['Grumpy', 'Sleepy'];
+
+        it('should return an error if a group that is less than 3 names is submitted.', function() {
+        	var sadLunchGroup = groupProcessor.makeGroups(entireStartUp);
+
+            expect(sadLunchGroup).to.be.an('error');
+        });
+
+        it('should return a shuffled group that has no more than 5 people, and no less than 3 people per group', function() {
+        	 	var perfectLunchGroups = groupProcessor.makeGroups(entireCompany);
+
+            expect(perfectLunchGroups[0].length).to.be.at.most(5);
+            expect(perfectLunchGroups[perfectLunchGroups.length -1].length).to.be.at.least(3);
+        });
+    });
 });
