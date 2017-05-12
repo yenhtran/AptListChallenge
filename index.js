603dda755161e5ff7d1d9a3f5691cd3503337feb
Yen Tran <yen.tran@autodesk.com>
2017-05-11 17:06:21 -0700

    refactors generateSummary

--- index.js
+++ index.js
@@ -17,41 +17,36 @@ console.log(
     )
 );
 
-var entireCompany = ['Happy', 'Dopey', 'Grumpy', 'Sneezy', 'Bashful', 'Sleepy', 'Doc', 'Snow', 'Super Woman', 'Batman', 'Superman', 'Thor', 'Loki'];
-
-
 process.stdin.on('data', function(data) {
-	fs.appendFile('data/input.txt', data, function(err) {
-		if (err) throw err;
+    fs.appendFile('data/input.txt', data, function(err) {
+        if (err) throw err;
 
-		readContent('input.txt', function(err, content) {
-			var allEmployees = content.split('\n');
+        readContent('input.txt', function(err, content) {
+            var allEmployees = content.split('\n');
 
-			allEmployees.splice(-1, 1);
-
-			var groups = groupProcessor.makeGroups(allEmployees);
-
-			groups.forEach(function(group, index){
-				process.stdout.write(`Group ${index + 1}` + '\n');
-				process.stdout.write(`=====\n`);
-				group.forEach(function(name) {
-					process.stdout.write(`${name}` + '\n');
-				});
-				process.stdout.write(`\n`);
-			});
-
-		});
-		// process.exit()
-	});
+            allEmployees.splice(-1, 1);
+            generateSummary(groupProcessor.makeGroups(allEmployees));
+       			process.exit()
+        });
+    });
 });
 
-
+function generateSummary(groups) {
+    groups.forEach(function(group, index) {
+        process.stdout.write(`Group ${index + 1}` + '\n');
+        process.stdout.write(`=====\n`);
+        group.forEach(function(name) {
+            process.stdout.write(`${name}` + '\n');
+        });
+        process.stdout.write(`\n`);
+    });
+}
 
 function readContent(fileName, callback) {
-	fs.readFile('data/input.txt', 'utf8', function(err, content) {
-		if(err) return console.log(err);
-		callback(null, content);
-	});
+    fs.readFile('data/input.txt', 'utf8', function(err, content) {
+        if (err) return console.log(err);
+        callback(null, content);
+    });
 }
 
 function beginProgram(i) {
